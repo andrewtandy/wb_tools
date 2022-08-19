0b2300e3e7d7147db7889bd6ff8ee2a1095a1c14
Andrew Tandy <andrewtandy3@gmail.com>
2022-08-19 09:00:55 +0100

    Found unique 4 digit code sent in auth email

--- outlook_access/test_outlook_access.py
+++ outlook_access/test_outlook_access.py
@@ -7,39 +7,56 @@ from datetime import datetime, timedelta
 outlook = win32com.client.Dispatch('outlook.application')
 mapi = outlook.GetNamespace("MAPI")
 
-# if multiple accounts in Outlook, need to pass account name when accessing folders, the following 
-# assumes only 1 account
-for account in mapi.Accounts:
-	print(account.DeliveryStore.DisplayName)
+'''
+ if multiple accounts in Outlook, need to pass account name when accessing folders, the following 
+ assumes only 1 account 
+'''
+# for account in mapi.Accounts:
+	# print(account.DeliveryStore.DisplayName)
 
 # TODO : dynamic input for specific account/folder access
 account_name = 'andrew.tandy@waterbabies.co.uk'
 
-# to access inbox folder, pass folder type (6 in this instance) - full list of folder types here:
-# https://docs.microsoft.com/en-us/office/vba/api/outlook.oldefaultfolders
-# 
-# subfolder access can be achieved with .Folders att. e.g. 'your_sub_folder':
-# inbox = mapi.GetDefaultFolder(6).Folders["your_sub_folder"]
-inbox = mapi.Folders(account_name).Folders('Inbox').Items
+'''
+ to access inbox folder, pass folder type (6 in this instance) - full list of folder types here:
+ https://docs.microsoft.com/en-us/office/vba/api/outlook.oldefaultfolders
 
-# for m in inbox:
-#     print(m)
+ subfolder access can be achieved with .Folders att. e.g. 'your_sub_folder':
+ inbox = mapi.GetDefaultFolder(6).Folders["your_sub_folder"]
+'''
+inbox = mapi.Folders(account_name).Folders('Inbox').Items
 
 # collect all messages from folder
 messages = inbox
 
-# message = messages.GetFirst()
-# body_content = message.body
+# set message filter types
+message_sender = 'no-reply@waterbabies.co.uk'
+message_subject = 'WaterBabies Authentication Code'
 
-# print(body_content)
-
-# filter messages using Restrict function
 received_dt = datetime.now() - timedelta(days=1)
 received_dt = received_dt.strftime('%m/%d/%Y %H:%M %p')
+
+# filter messages using Restrict function
 messages = messages.Restrict("[ReceivedTime] >= '" + received_dt + "'")
-messages = messages.Restrict("[SenderEmailAddress] = 'no-reply@waterbabies.co.uk'")
-messages = messages.Restrict("[Subject] = 'WaterBabies Authentication Code'")
+messages = messages.Restrict(f"[SenderEmailAddress] = '{ message_sender }'")
+messages = messages.Restrict(f"[Subject] = '{ message_subject }'")
+
+# use GetFirst to locate latest mail received. Consider loop if need to access more than
+# 1 message
+message = messages.GetFirst()
+
+
+body = message.body
+# use the known word 'Account' to find index in body and pull auth code
+keyword = body.index("Account.")
+code = body[keyword+12:keyword+16]
+print(code)
+
 
 # print all messages subject to filter criteria
-for message in list(messages):
-    print(message.body)
+# for message in list(messages):
+#     body = message.body
+#     # use the known word 'Account' to find index in body and pull auth code
+#     keyword = body.index("Account.")
+#     code = body[keyword+12:keyword+16]
+#     print(code)
